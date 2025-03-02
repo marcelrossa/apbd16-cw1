e9ba2ed6336522ab985f8b6ccdad8041966fa096
Marcel Rosiński <s22747@pjwstk.edu.pl>
2025-03-02 16:51:07 +0100

    Modyfikacja 4

--- Zadanie2/Zadanie2/Program.cs
+++ Zadanie2/Zadanie2/Program.cs
@@ -3,3 +3,12 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1!");
 Console.WriteLine("Modyfikacja 2!");
 Console.WriteLine("Modyfikacja 3!");
+
+int[] arr = { 1, 2, 3, 4, 5 };
+
+double GetAverage(int[] numbers)
+{
+    return 0;
+}
+
+GetAverage(arr);
